13be07624607447d017ec42215c04dec188a2391
quanjiaqi <645316110@qq.com>
2021-06-27 18:28:08 +0800

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent, NewsComponent, ProductsComponent } from './componemts';
+const routes: Routes = [{
+  path:'',
+  redirectTo:'/home',
+  pathMatch:'full'
+},{
+  path:'/home',
+  component:HomeComponent
+},{
+  path:'/news',
+  component:NewsComponent
+},{
+  path:'/products',
+  component:ProductsComponent
+},{
+  path:'**',
+  component:HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent, NewsComponent, ProductsComponent } from './componemts';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NewsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
